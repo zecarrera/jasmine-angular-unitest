1fce2193efc52f74c6034243e773fd8b8c16bc47
Jose Neto <jose.neto@bglgroup.co.uk>
2018-06-25 17:21:05 +0100

    Adds wdio test setup files

---
+++ scripts/wdio.conf.js
@@ -0,0 +1,28 @@
+exports.config = {
+    specs: [
+        './../app/js/test/ui/specs/*.js'
+    ],
+    exclude: [],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'firefox'
+    }],
+    sync: true,
+    logLevel: 'silent',
+    coloredLogs: true,
+    deprecationWarnings: true,
+    bail: 0,
+    screenshotPath: './app/js/test/ui/specs/errorShots/',
+    baseUrl: 'http://localhost:5000',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 10000,
+        expectationResultHandler: function(passed, assertion) {
+        }
+    },
+   
+}
