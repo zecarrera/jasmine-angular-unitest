500c9c2b00a7c9b9d082e29900359bb273204905
Jose Neto <jose.neto@bglgroup.co.uk>
2018-06-25 17:21:18 +0100

    Adds scripts to start server and run ui tests

---
+++ scripts/run-ui-tests.sh
@@ -0,0 +1,2 @@
+echo "*** Starting wdio UI tests ***"
+./../node_modules/.bin/wdio wdio.conf
---
+++ scripts/start-selenium-server.sh
@@ -0,0 +1,2 @@
+echo "*** Starting selenium server ***"
+java -jar -Dwebdriver.gecko.driver=./../app/js/test/ui/geckodriver ./../app/js/test/ui/selenium-server-standalone-3.12.0.jar
