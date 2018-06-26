896c961476c5baef009b86e8a1fe47259de979f5
Jose Neto <jose.neto@bglgroup.co.uk>
2018-06-26 23:15:34 +0100

    Date is now entered - but still not valid

--- app/js/test/ui/specs/ticketsUiSpec.js
+++ app/js/test/ui/specs/ticketsUiSpec.js
@@ -6,20 +6,19 @@ describe('When user enters valid data to apply for a discount', function () {
         browser.url('http://localhost:5000');
         browser.setValue("#firstname", "jose");
         browser.setValue("#lastname", "carrera");
-        // browser.click("#dob");
-        // browser.click("#dob");
-        browser.addValue("#dob", "11/10/1900");
-        // browser.keys(["Escape", "Digit1"]);
-        // browser.keys("Digit1");
-        // browser.setValue("#dob", formatDate("11/10/1900"));
-        // browser.element('form').click('"#dob"').keys("11/10/1900");
 
-        // browser.keys("#dob", ["0", "2"]);
+        var date = '2000-03-28';
+        browser.execute(function (date) {
+            document.querySelector('#dob').value = date;
+        }, date);
+        
         browser.click("#apply-button");
     });
 
     it('Then application result shows a 15% discount', function () {
-        var discount = browser.getText("#result-discount");
+        // browser.waitForVisible("#result-discount", 3000);
+        // var discount = browser.getText("#result-discount");
+        browser.pause(5000);
         // assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
         expect(discount).toEqual("15");
     });
